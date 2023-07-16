9645a0aed83cee4fe978ffd95fec9f9bf2786b1e
sarah441 <sarahibrahim3477@gmail.com>
2023-07-16 08:20:21 -0700

    :dfsf:

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,13 @@
 */
 int main(void)
 {
+
 printf("Size of int: %lu\n", sizeof(int));
 printf("Size of char: %lu\n", sizeof(char));
 printf("Size of float: %lu\n", sizeof(float));
 printf("Size of double: %lu\n", sizeof(double));
 printf("Size of long int: %lu\n", sizeof(long int));
 printf("Size of long long int: %lu\n", sizeof(long long int));
-Return 0;
+return 0;
+
 }
