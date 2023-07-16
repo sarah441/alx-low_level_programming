0b935ab476806a62df0b5a5230b9942c7b9a9f32
sarah441 <sarahibrahim3477@gmail.com>
2023-07-16 07:56:22 -0700

    kjkj

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+* description: A C programm that prints with printf function
+*
+* Return: always 0 (success)
+*/
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+Return(0);
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Prints the size of various types.
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+printf("Size of int: %lu\n", sizeof(int));
+printf("Size of char: %lu\n", sizeof(char));
+printf("Size of float: %lu\n", sizeof(float));
+printf("Size of double: %lu\n", sizeof(double));
+printf("Size of long int: %lu\n", sizeof(long int));
+printf("Size of long long int: %lu\n", sizeof(long long int));
+Return 0;
+}
