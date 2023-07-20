93626c8d0b077194fb633b687c96a637b3f50fc5
sarah441 <sarahibrahim3477@gmail.com>
2023-07-20 06:14:11 -0700

    QWeqewqew

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *description : check if number is positive or negative
+  *Return:  Always 0
+*/
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+}
