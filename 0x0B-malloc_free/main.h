c16a6e6935d841c89ca579dda5c1be6244139f48
sarah441 <sarahibrahim3477@gmail.com>
2023-08-07 07:29:45 -0700

    arwrwer

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /* MAIN_H */
