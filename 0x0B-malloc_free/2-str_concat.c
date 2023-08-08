95f2b4b720a047752eaf4485cfbde11d398c306c
sarah441 <sarahibrahim3477@gmail.com>
2023-08-08 13:40:00 -0700

    ewrwrew

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - create an array of chars
+ * @s1: the size of the array
+ * @s2: the charachter
+ *
+ * Return: the two strings concatenated
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[i])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	j = 0;
+
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
+	}
+
+	s[k] = '\0';
+	return (s);
+}
