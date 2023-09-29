757e51c8fbd58683f9f7d5c71928d2545fe8d5f4
sarah441 <you@example.com>
2023-09-29 09:18:47 -0700

    dsada

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _putchar(char c);
+int _putchar(char c)
+{
+  return write(1, &c, 1);
+}
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,3 +12,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+
