479abf978d2799cb29f66a2b27c1e23772fe3132
sarah441 <you@example.com>
2023-10-05 23:27:07 -0700

    asddaf

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper char
+ * @c: the char
+ * Return: 1 if c is upper, 0 if not
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
